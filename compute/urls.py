7bb45e3cb977a4d96d4f2e46bb8b31bbe4afd568
jbyrnes <jbyrnes@adeptusproductions.com>
2015-01-21 09:00:42 +1000

    save new task

--- compute/urls.py
+++ compute/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
 
        url(r'^tasks$', views.tasks, name='tasks'),
 
-       url(r'^task$', views.task, name='task'),
-           url(r'^task/(?P<task_id>\d+)/$', views.task, name='task'),
+       url(r'^task/(?P<task_id>\d+)/$', views.task, name='task'),
+
+       url(r'^new_task$', views.new_task, name='new_task'),
+       url(r'^save_task$', views.save_task, name='save_task'),
 )
--- compute/views.py
+++ compute/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from compute.models import DistributedTask
@@ -13,8 +13,23 @@ def tasks(request):
     tasks = DistributedTask.objects.all
     return render(request, 'compute/tasks.html', {'tasks': tasks})
 
-def task(request):
+def task(request, task_id):
     task = get_object_or_404(DistributedTask, pk=task_id)
     # question = Question.objects.get(pk=question_id)
+    return render(request, 'compute/task.html', {'task': task})
+
+def new_task(request):
+    task = DistributedTask()
+    # question = Question.objects.get(pk=question_id)
+    return render(request, 'compute/task.html', {'task': task})
+
+def save_task(request):
+    task = DistributedTask()
+    task.name = "task"
+    task.map_func = request.POST.get("map_func")
+    task.reduce_func = request.POST.get("reduce_func")
+    task.then_func = request.POST.get("then_func")
+    task.data = request.POST.get("data") or ""
+    task.save()
     return render(request, 'compute/task.html', {'task': task})
 
